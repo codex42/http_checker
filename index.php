0bc1e7440dd7615bf801ca98cac0f11c89d2c1b0
root <root@ns382737.ip-46-105-101.eu>
2017-11-28 11:43:28 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+$config_file = file_get_contents('config/sites.json');
+$sites=json_decode($config_file);
+$idSite=$_GET['id'];
+$err='';
+
+
+class Site
+{
+  public  $sid;
+  public  $sData;
+
+ 
+ 	function __construct($sid=null,$sData=null) {
+        $this->sid=$sid;
+        $this->sData=$sData;
+    }	
+         
+ 
+  public function check()
+  {
+    //print_r($this->sData) ;
+    foreach ($this->sData->urls as $key => $u) {	
+    	$request = curl_init();
+		curl_setopt($request, CURLOPT_URL, $u->url);
+		curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($request, CURLOPT_HEADER,1);
+		$result = curl_exec ($request);
+		curl_close($request);
+
+		$pos = strpos($result, 'HTTP/1.1 200 OK');
+
+		if ($pos === false) {
+		    $this->sData->urls[$key]->status="FALSE";
+		} else {
+		    $this->sData->urls[$key]->status="TRUE";
+		}
+			
+		foreach ($u->keywords as $kkey => $k) {	
+			$pos = strpos($result, $k->keyword);
+
+			if ($pos === false) {
+			     $this->sData->urls[$key]->keywords[$kkey]->status="FALSE";
+			} else {
+			    $this->sData->urls[$key]->keywords[$kkey]->status="TRUE";
+			}
+		}
+
+    }
+
+    $this->sData->logs->SERVER_ADDR=$_SERVER['SERVER_ADDR'];
+    $this->sData->logs->HTTP_HOST=$_SERVER['HTTP_HOST'];
+    $this->sData->logs->REQUEST_TIME=$_SERVER['REQUEST_TIME'];
+    $this->sData->logs->REQUEST_TIME_h=date(DATE_ATOM,$_SERVER['REQUEST_TIME']);
+    echo json_encode($this->sData,JSON_UNESCAPED_SLASHES);	
+  }
+}
+
+
+	$pattern='/\w+$/';
+	if (!empty($idSite) && preg_match($pattern,$idSite)){
+		foreach($sites->sites as $site)
+		{	
+
+		    if($site->site->id == $idSite)
+		    {
+		        $checkSite=new Site($idSite,$site->site);
+		        $checkSite->check();
+		        $err='';
+		        break;
+		    }
+		    else{
+		    	$err='Illegal ID';
+		    }
+		}
+	}else{
+		$err='Illegal ID';
+	}
+	echo $err;
+
+
+?>
